6c0bcc303c2a2e0a83424d1a659ae75cc24f4abc
Siarhei Yakushevich <yakushevichsv@hotmail.com>
2020-12-13 14:37:38 +0300

    Initial Commit

---
+++ CurrencyConvertion/CurrencyConvertionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertionApp.swift
+//  CurrencyConvertion
+//
+//  Created by Siarhei Yakushevich on 13.12.20.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
